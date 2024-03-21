d4145722c04d9b7e504da5f021b88ba1d106007d
sanusi-pohlor <134247007+sanusi-pohlor@users.noreply.github.com>
2024-03-21 17:23:40 +0700

    210367.2

--- src/components/Admin/ManageContent/Adm_News/Adm_News_Form.js
+++ src/components/Admin/ManageContent/Adm_News/Adm_News_Form.js
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useState } from "react";
 import AdminMenu from "../../Adm_Menu";
-import "react-quill/dist/quill.snow.css";
+import 'quill/dist/quill.snow.css'
 import { Form, Input, Button, message, Upload, Card, Select } from "antd";
 import ReactQuill from "react-quill";
 import { PlusOutlined, UserOutlined } from "@ant-design/icons";
@@ -73,43 +73,27 @@ const Adm_News_Form = () => {
     fetchUser();
   }, []);
 
-  const modules = {
-    toolbar: {
-      container: [
-        [
-          { header: "1" },
-          { header: "2" },
-          { header: [3, 4, 5, 6] },
-          { font: [] },
-        ],
-        [{ size: [] }],
-        ["bold", "italic", "underline", "strike", "blockquote"],
-        [{ list: "ordered" }, { list: "bullet" }],
-        ["link"],
-        ["clean"],
-        ["code-block"],
-        ["image"],
-        [{ align: [] }],
+  var modules = {
+    toolbar: [
+      [{ size: ["small", false, "large", "huge"] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [{ list: "ordered" }, { list: "bullet" }],
+      ["link", "image"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+        { align: [] }
       ],
-    },
-    clipboard: {
-      matchVisual: true,
-    },
+      [{ "color": ["#000000", "#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc", "#9933ff", "#ffffff", "#facccc", "#ffebcc", "#ffffcc", "#cce8cc", "#cce0f5", "#ebd6ff", "#bbbbbb", "#f06666", "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff", "#888888", "#a10000", "#b26b00", "#b2b200", "#006100", "#0047b2", "#6b24b2", "#444444", "#5c0000", "#663d00", "#666600", "#003700", "#002966", "#3d1466", 'custom-color'] }],
+    ]
   };
-  const formats = [
-    "header",
-    "font",
-    "size",
-    "bold",
-    "italic",
-    "underline",
-    "strike",
-    "blockquote",
-    "list",
-    "bullet",
-    "link",
-    "image",
-    "align",
+  var formats = [
+    "header", "height", "bold", "italic",
+    "underline", "strike", "blockquote",
+    "list", "color", "bullet", "indent",
+    "link", "image", "align", "size",
   ];
 
   const handleChange = (html) => {
@@ -253,6 +237,7 @@ const Adm_News_Form = () => {
               <ReactQuill
                 onChange={handleChange}
                 placeholder={createTypography("เพิ่มรายละเอียดเพิ่มเติม")}
+                theme="snow"
                 formats={formats}
                 modules={modules}
                 style={{ height: "950px" }}
